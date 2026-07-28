6e3510db8ae25b47d47230db2c126f0c1e49d303
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2026-07-28 14:02:03 -0400

    simplifed version of RobotState compared to 2026

--- src/main/java/frc/robot/RobotState.java
+++ src/main/java/frc/robot/RobotState.java
@@ -1,5 +1,41 @@
 package frc.robot;
 
+import java.util.Optional;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+
 public class RobotState {
-    
+    private Pose2d fieldToRobot = Pose2d.kZero;
+    private double lastTimestamp = Double.MIN_NORMAL;
+
+    private ChassisSpeeds measuredFieldRelativeChassisSpeeds = new ChassisSpeeds();
+
+    public RobotState() {}
+
+    public void addPoseObservation(double timestamp, Pose2d robotPose, ChassisSpeeds robotRelativeChassisSpeeds, ChassisSpeeds fieldRelativeChassisSpeeds, double yaw, double yawRate, double yawAcceleration) {
+        updateRobotPoseIfNewer(timestamp, robotPose);
+        measuredFieldRelativeChassisSpeeds = fieldRelativeChassisSpeeds;
+    }
+
+    private void updateRobotPoseIfNewer(double timestamp, Pose2d pose) {
+        if (timestamp > lastTimestamp) {
+            lastTimestamp = timestamp;
+            fieldToRobot = pose;
+        }
+    }
+
+    public Pose2d getFieldToRobotPose() {
+        return fieldToRobot;
+    }
+
+    public ChassisSpeeds getMeasuredFieldRelativeChassisSpeeds() {
+        return measuredFieldRelativeChassisSpeeds;
+    }
+
+    public boolean isRedAlliance() {
+        return DriverStation.getAlliance().isPresent() && DriverStation.getAlliance().equals(Optional.of(Alliance.Red));
+    }
 }
