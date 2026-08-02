dfa86b167e2f6125ec838feba2bb773c85c14257
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2026-08-02 17:01:03 -0400

    switched to LoggedRobot

--- src/main/java/first/robot/Robot.java
+++ src/main/java/first/robot/Robot.java
@@ -4,9 +4,23 @@
 
 package first.robot;
 
+import org.littletonrobotics.junction.LogFileUtil;
 import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+
 import org.wpilib.command3.Command;
 import org.wpilib.command3.Scheduler;
+
+import com.ctre.phoenix6.SignalLogger;
+
+import first.BuildConstants;
+import first.Constants;
+import first.minolib.advantagekit.LoggedTracer;
+import first.minolib.phoenix.PhoenixUtility;
+
 public class Robot extends LoggedRobot {
   private Command autonomousCommand;
 
@@ -14,11 +28,48 @@ public class Robot extends LoggedRobot {
 
   public Robot() {
     robotContainer = new RobotContainer();
+
+    Logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
+    Logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
+    Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
+    Logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
+    Logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
+    Logger.recordMetadata("GitDirty", switch(BuildConstants.DIRTY) {
+        case 0 -> "All changes committed";
+        case 1 -> "Uncommitted changes";
+        default -> "Unknown";
+    });
+
+    switch(Constants.getMode()) {
+      case REAL:
+        Logger.addDataReceiver(new WPILOGWriter());
+        Logger.addDataReceiver(new NT4Publisher());
+        break;
+      case SIM:
+        Logger.addDataReceiver(new NT4Publisher());
+        break;
+      case REPLAY:
+        setUseTiming(false);
+        String path = LogFileUtil.findReplayLog();
+        Logger.setReplaySource(new WPILOGReader(path));
+        Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(path, "_sim")));
+        break;
+    }
+
+    SignalLogger.setPath("/media/sda1/");
+    SignalLogger.enableAutoLogging(false);
+    Logger.start();
   }
 
   @Override
   public void robotPeriodic() {
+    LoggedTracer.reset();
+
+    PhoenixUtility.refreshAll();
+    LoggedTracer.record("PhoenixRefresh");
+
     Scheduler.getDefault().run();
+    LoggedTracer.record("Scheduler");
   }
 
   @Override
