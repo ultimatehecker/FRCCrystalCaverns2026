f9b120e571c33a8b6d1917104414002fe29a0a08
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2026-08-02 18:12:14 -0400

    added RobotContainer

--- src/main/java/first/robot/RobotContainer.java
+++ src/main/java/first/robot/RobotContainer.java
@@ -4,8 +4,19 @@
 
 package first.robot;
 
+import java.util.function.Consumer;
+
 import org.wpilib.command3.Command;
-import org.wpilib.driverstation.Gamepad;
+
+import first.minolib.vision.FieldPoseEstimation;
+import first.robot.constants.DrivetrainConstants;
+import first.robot.oi.DriverController;
+import first.robot.oi.DriverControllerXbox;
+import first.robot.simulation.SimulatedRobotState;
+import first.robot.subsystems.drivetrain.Drivetrain;
+import first.robot.subsystems.drivetrain.DrivetrainIOHardware;
+import first.robot.subsystems.drivetrain.DrivetrainIOSimulation;
+import first.robot.subsystems.drivetrain.TunerConstants;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -14,26 +25,79 @@ import org.wpilib.driverstation.Gamepad;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
+  private RobotState robotState;
+  private SimulatedRobotState simulatedRobotState;
+
+  private Drivetrain drivetrain;
+
+  private DriverController controller;
+
+  private RobotState buildRobotState() {
+    if (Robot.isSimulation()) {
+      return new RobotState(visionEstimateConsumer);
+    } else {
+      return new RobotState(visionEstimateConsumer);
+    }
+  }
+
+  private SimulatedRobotState buildSimulatedRobotState() {
+    if (Robot.isSimulation()) {
+      return new SimulatedRobotState(this);
+    } else return null;
+  }
+
+  @SuppressWarnings("unchecked")
+  private Drivetrain buildDrivetrain() {
+    if (Robot.isSimulation()) {
+      return new Drivetrain(robotState, new DrivetrainIOSimulation(robotState, simulatedRobotState, DrivetrainConstants.kDrivetrain.getDriveTrainConstants(), TunerConstants.kFrontLeft, TunerConstants.kFrontRight, TunerConstants.kBackLeft, TunerConstants.kBackRight));
+    } else {
+      return new Drivetrain(robotState, new DrivetrainIOHardware(robotState, DrivetrainConstants.kDrivetrain.getDriveTrainConstants(), TunerConstants.kFrontLeft, TunerConstants.kFrontRight, TunerConstants.kBackLeft, TunerConstants.kBackRight));
+    }
+  }
+
+  public RobotState getRobotState() {
+    return robotState;
+  }
+
+  public SimulatedRobotState getSimulatedRobotState() {
+    return simulatedRobotState;
+  }
+
+  public Drivetrain getDrivetrain() {
+    return drivetrain;
+  }
+
+  public DriverController buildDriverController() {
+    return new DriverController(new DriverControllerXbox());
+  }
+
   public RobotContainer() {
+    robotState = buildRobotState();
+    simulatedRobotState = buildSimulatedRobotState();
+
+    drivetrain = buildDrivetrain();
+
+    controller = buildDriverController();
+
     configureButtonBindings();
   }
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link org.wpilib.driverstation.GenericHID} or one of its subclasses ({@link
-   * org.wpilib.driverstation.Joystick} or {@link Gamepad}), and then passing it to a {@link
-   * org.wpilib.command3.button.JoystickButton}.
-   */
   private void configureButtonBindings() {
-    
+    drivetrain.setDefaultCommand(drivetrain.drive(controller::getThrottle, controller::getStrafe, controller::getRotation, controller::toggleDrivingMode));
   }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
     return null;
   }
+
+  private final Consumer<FieldPoseEstimation> visionEstimateConsumer = new Consumer<FieldPoseEstimation>() {
+    @Override
+    public void accept(FieldPoseEstimation estimate) {
+        drivetrain.addVisionMeasurement(estimate);
+    }
+  };
 }
